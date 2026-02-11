6129e166d2ebca48bb47133e7a656241169b7b04
Mansur <165130890+Mansurrv@users.noreply.github.com>
2026-02-11 11:11:46 +0500

    Delete src directory

--- src/routes/city.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const cityController = require("../controllers/city.controller");
-
-router.get("/", cityController.getCities);
-router.post("/", cityController.addCity);
-
-module.exports = router;
